d8a5bed99e63429f7a9f8baee25bf1df36979166
achan <Alexander@arekisannda.com>
2016-12-26 23:09:54 -0800

    Adds ft_gnl, get_next_line, to the library

---
+++ ft_io/ft_gnl.c
@@ -0,0 +1,127 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_gnl.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achan <achan@student.42.us.org>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/26 23:01:15 by achan             #+#    #+#             */
+/*   Updated: 2016/12/26 23:01:49 by achan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_io.h"
+
+t_fd_state	*fd_new(int fd)
+{
+	t_fd_state	*ret;
+
+	if (fd < 0)
+		return (NULL);
+	if (!(ret = (t_fd_state *)malloc(sizeof(t_fd_state))))
+		return (NULL);
+	ret->fd = fd;
+	ret->rem_size = 0;
+	ft_bzero(ret->rem, BUFF_SIZE);
+	return (ret);
+}
+
+int			fd_cmp(void *fd_state, void *fd)
+{
+	t_fd_state	*tmp_state;
+	int			*tmp_fd;
+
+	if (!fd_state)
+		return (0);
+	tmp_state = (t_fd_state *)fd_state;
+	tmp_fd = (int *)fd;
+	if (tmp_state->fd == *tmp_fd)
+		return (1);
+	return (0);
+}
+
+static int	get_rem_line(char **line, t_fd_state *state, size_t *cur_size)
+{
+	char	*nl_pos;
+	size_t	d_size;
+
+	*cur_size = 0;
+	if (state->rem_size > 0)
+	{
+		free(*line);
+		if ((nl_pos = ft_memchr(state->rem, '\n', state->rem_size)))
+		{
+			d_size = nl_pos - state->rem;
+			*cur_size = (state->rem_size -= (d_size + 1));
+			*line = (char *)ft_memndup(state->rem, d_size + 1);
+			(*line)[d_size] = 0;
+			ft_memmove(state->rem, state->rem + d_size + 1, *cur_size);
+			return (1);
+		}
+		else
+		{
+			*cur_size = state->rem_size;
+			*line = ft_memndup(state->rem, *cur_size + 1);
+			(*line)[*cur_size] = 0;
+			state->rem_size = 0;
+			ft_bzero(state->rem, BUFF_SIZE);
+		}
+	}
+	return (0);
+}
+
+static int	get_read_line(char **line,
+							t_fd_state *state, size_t *cur_size)
+{
+	int		ret;
+	char	*nl_pos;
+	size_t	d_size;
+	char	buf[BUFF_SIZE];
+
+	while ((ret = read(state->fd, buf, BUFF_SIZE)) > 0)
+	{
+		if ((nl_pos = ft_memchr(buf, '\n', ret)))
+		{
+			d_size = nl_pos - buf;
+			state->rem_size = ret - (d_size + 1);
+			ft_memmove(state->rem, buf + d_size + 1, state->rem_size);
+		}
+		else
+			d_size = ret;
+		if (!(*line = ft_remalloc(*line, *cur_size, *cur_size + d_size + 1)))
+			return (-1);
+		ft_memmove(*line + *cur_size, buf, d_size);
+		*cur_size += d_size;
+		(*line)[*cur_size] = 0;
+		if (nl_pos || ret < BUFF_SIZE)
+			return (1);
+	}
+	return (ret);
+}
+
+int			ft_gnl(const int fd, char **line)
+{
+	static t_llist	fd_states = {NULL, NULL};
+	t_fd_state		*state;
+	size_t			cur_size;
+	int				read_status;
+
+	if (!line || fd < 0)
+		return (-1);
+	if (!(*line = (char *)malloc(sizeof(char))))
+		return (-1);
+	line[0][0] = 0;
+	if (ft_llst_empty(&fd_states) ||
+		!(state = (ft_llst_find(&fd_states, (int *)&fd, &fd_cmp))))
+		ft_llst_pushbck(&fd_states, (state = fd_new(fd)), sizeof(t_fd_state));
+	if (get_rem_line(line, state, &cur_size))
+		return (1);
+	read_status = get_read_line(line, state, &cur_size);
+	read_status = (read_status >= 0 && cur_size > 0) ? 1 : read_status;
+	if (!read_status && !state->rem_size)
+	{
+		ft_llst_idel(&fd_states, &(state->fd), (*fd_cmp));
+		state = NULL;
+	}
+	return (read_status);
+}
--- includes/ft_io.h
+++ includes/ft_io.h
@@ -6,7 +6,7 @@
 /*   By: achan <achan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/22 05:44:20 by achan             #+#    #+#             */
-/*   Updated: 2016/12/23 15:41:23 by achan            ###   ########.fr       */
+/*   Updated: 2016/12/26 23:03:49 by achan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define FT_IO_H
 
 # include <unistd.h>
+# include "ft_linkedlist.h"
+# include "ft_general.h"
 
 int					ft_utf_encode(int c, int *output);
 
@@ -35,4 +37,16 @@ void				ft_putendl_w_fd(char const *s, int fd);
 void				ft_putnbr(int n);
 void				ft_putnbr_fd(int n, int fd);
 
+# define BUFF_SIZE	4096
+
+typedef struct		s_fd_state
+{
+	int				fd;
+	size_t			rem_size;
+	char			rem[BUFF_SIZE];
+}					t_fd_state;
+
+int					ft_gnl(const int fd, char **line);
+int					fd_cmp(void *fd_state, void *fd);
+t_fd_state			*fd_new(int fd);
 #endif
